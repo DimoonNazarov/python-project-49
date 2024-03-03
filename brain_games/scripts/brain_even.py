1769ac2b501a466eab246f5b6a7bab3ae643f67a
dimoon <nazarovdima39@mail.ru>
2024-03-03 14:39:08 +0300

    исправил ошибки

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
 import random
 import prompt
+from brain_games.cli import welcome_user
 
 
 def brain_noteven():
+    name = welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
     k = 0
     for i in range(3):
-        x = random.randint(1, 100)
-        print(f"Question: {x}")
+        random_number = random.randint(1, 100)
+        print(f"Question: {random_number}")
         answer = prompt.string("Your answer: ")
-        if (x % 2 == 0 and answer == 'yes') or (x % 2 != 0 and answer == 'no'):
+        if (random_number % 2 == 0 and answer == 'yes') or (random_number % 2 != 0 and answer == 'no'):
             print("Correct!")
             k = k + 1
             if k == 3:
-                print("Congratulations, Bill!")
+                print(f"Congratulations, {name}!")
         else:
-            print("'yes' is wrong answer ;(.'\n'Correct answer was 'no'. Let's try again, Bill!")
+            print(f"'yes' is wrong answer ;(.\nCorrect answer was 'no'. Let's try again, {name}!")
             break
 
 
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 
 
-from brain_games.cli import welcome_user
 from brain_games.scripts.brain_even import brain_noteven
 
 
 def main():
     print("Welcome to the Brain Games!")
-    welcome_user()
     brain_noteven()
-
 
 
 if __name__ == '__main__':
