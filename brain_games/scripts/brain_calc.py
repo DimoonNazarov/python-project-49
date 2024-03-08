fc3c9dc6252f2ae631572888e632491812b7cbd7
dimoon <nazarovdima39@mail.ru>
2024-03-08 20:24:30 +0300

    updated calc function. fixed error

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,19 +1,20 @@
-from brain_games.cli import welcome_user
+#!/usr/bin/env python3
 import random
 import prompt
+import brain_games.cli
 
 
 def calc():
-    name = welcome_user()
+    name = brain_games.cli.welcome_user()
     print('What is the result of the expression?')
     k = 0
     for i in range(3):
         first, second = random.randint(1, 20), random.randint(1, 20)
-        
-        real_answer= f"Question: {first} {random.choice('-*+')} {second}"
+        print("Question: ", end='')
+        real_answer= f"{first} {random.choice('-*+')} {second}"
         print(real_answer)
         answer = prompt.integer("Your answer: ")
-        if  eval(real_answer) == int(answer):
+        if eval(real_answer) == answer:
             print("Correct!")
             k = k + 1
             if k == 3:
@@ -22,8 +23,10 @@ def calc():
             print(f"'{answer}' is wrong answer ;(. Correct answer was '{eval(real_answer)}'.")
             break
 
+
 def main():
     calc()
+
 
 if __name__ == "__main__":
     main()
