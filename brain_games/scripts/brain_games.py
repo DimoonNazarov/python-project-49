ce47e0ae181ee05c13f86fc8ba8ebc6510ed7805
dimoon <nazarovdima39@mail.ru>
2024-03-11 18:56:36 +0300

    поменял на функцию

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -4,9 +4,7 @@ import prompt
 
 
 def main():
-    print('Welcome to the Brain Games!\n')
-    name = prompt.string('May I have your name? ')
-    print('Hello, {0}!'.format(name))
+   name = brain_games.cli.welcome_user()
     
 
 if __name__ == '__main__':
